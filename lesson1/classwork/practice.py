43f39da3457c9355421a50fa7e0875f2d3480dda
kimanyaubbey-cpu <kimanyaubbey@gmail.com>
2026-07-08 21:37:27 +0000

    solution

--- lesson1/classwork/practice.py
+++ lesson1/classwork/practice.py
@@ -1,24 +1,34 @@
 # Problem 1
 # Create a variable for your favorite number and print it.
-
+favorite_number = 24
+print(favorite_number)
 
 
 # Problem 2
 # Create a variable for your height in meters and print it.
-
+height = 2 
+print(height)
 
 
 # Problem 3
 # Create a variable for your favorite animal and print it in a full sentence.
-
+favorite_animal = "jaguar"
+print("My favorite animal is a ", favorite_animal)
 
 
 # Problem 4
 # Create two variables: one for your first name and one for your last name.
 # Print them on 2 separate lines.
-
+first_name = "Kimanya"
+last_name = "Ubbey" 
+print(first_name)
+print(last_name)
 
 
 # Problem 5
 # Create 3 variables: your first name, your age, and your favorite color.
 # Print them on the same line in a full sentence.
+first_name = "Kimanya"
+age = "13 years"
+favorite_color = " Midnight blue"
+print("My name is", first_name + "I am", age, "years old, and my favorite color is", favorite _color + ",")
---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,14 @@
+x = 5 # integer
+print(x)
+
+number = 3.98 # float
+print(number)
+
+favorite_food = "pizza" # string (text)
+print("My favorite food is " + favorite_food)
+print("My favorite food is", favorite_food)
+
+cost = 10
+print("This burger costs $" + str(cost))
+
+print(4.21, "dogs", x)
